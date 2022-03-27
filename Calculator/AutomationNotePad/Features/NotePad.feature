c508539ec4e880042c8f15bc93aeec3194def35c
Flaviu Tritean <flaviu.pk@gmail.com>
2022-03-27 21:33:24 +0300

    Automation cu Specflow si FLaUi

---
+++ Calculator/AutomationNotePad/Features/NotePad.feature
@@ -0,0 +1,12 @@
+﻿Feature: Calculator
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](AutomationNotePad/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+Scenario: Enter text in note pad, clik X and don's save
+	Given open notepad
+	When the text is enterd
+	Then press X and don't save it
---
+++ Calculator/AutomationNotePad/Steps/NotePadStepDefinitions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using FlaUI.Core;
+using FlaUI.UIA3;
+using FlaUI.UIA2;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.AutomationElements;
+using System.Threading;
+
+namespace AutomationNotePad.Features
+{
+    [Binding]
+    public class NotePadStepDefinitions
+    {
+        Application application = new Application();
+
+        ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+
+        [Given(@"open notepad")]
+        [Obsolete]
+        public void GivenOpenNotepad()
+        {
+            
+            
+        }
+
+        var mainwindow = application.GetMainWindow(new UIA3Automation());
+
+        [When(@"the text is enterd")]
+        [Obsolete]
+        public void WhenTheTextIsEnterd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"press X and don't save it")]
+        [Obsolete]
+        public void ThenPressXAndDonTSaveIt()
+        {
+            mainwindow.FindFirstDescendant(cf.ByName("Text Editor")).AsTextBox().Enter("Salut. Ce mai faci?");
+        }
+    }
+}
