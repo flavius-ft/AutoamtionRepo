887e6a911be49c6df7185321eae114b8a198a995
Flaviu Tritean <flaviu.pk@gmail.com>
2022-04-09 20:21:56 +0300

    specflow nu functioneaza inca, nu pot instantia o anumita clasa

--- Calculator/AutomationNotePad/Steps/NotePadStepDefinitions.cs
+++ Calculator/AutomationNotePad/Steps/NotePadStepDefinitions.cs
@@ -6,7 +6,7 @@ namespace AutomationNotePad.Features
     [Binding]
     public class NotePadStepDefinitions
     {
-       
+        AutomationNoteP pad = new();     // nu pot instantia clasa 
 
         [Given(@"open notepad")]
         [Obsolete]
