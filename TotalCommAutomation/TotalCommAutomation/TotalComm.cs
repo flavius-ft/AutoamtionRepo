b00839d62418383cc0b32dfd094e46e6111c70c3
Flaviu Tritean <flaviu.pk@gmail.com>
2022-05-03 07:58:55 -0700

    download functioneaza

--- TotalCommAutomation/TotalCommAutomation/TotalComm.cs
+++ TotalCommAutomation/TotalCommAutomation/TotalComm.cs
@@ -18,10 +18,11 @@ namespace TotalCommSpecflow
         private static readonly HttpClient client = new();
 
         //"https://www.totalcommander.ch/beta/tc1050x64_b1.exe"))
+        // https://www.ghisler.com/
         public static async Task Main()
         {
-            using var stream = await client.GetStreamAsync("https://www.totalcommander.ch/beta/tc1050x64_b1.exe");
-            using var fileStream = new FileStream(@"C:\Users\User\Downloads\TotalCommand", FileMode.CreateNew);
+            using var stream = await client.GetStreamAsync(@"https://www.totalcommander.ch/beta/tc1050x64_b1.exe");
+            using var fileStream = new FileStream(@"C:\Users\User\Downloads\Automation\tc1050x64_b1.exe", FileMode.CreateNew);
             await stream.CopyToAsync(fileStream);
         }
 
