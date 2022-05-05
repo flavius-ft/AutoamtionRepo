6ce59c04f91d38aa80486eb81800839d13761ba9
Flaviu Tritean <flaviu.pk@gmail.com>
2022-05-05 07:45:40 -0700

    try install the app

--- TotalCommAutomation/TotalCommAutomation/TotalComm.cs
+++ TotalCommAutomation/TotalCommAutomation/TotalComm.cs
@@ -16,16 +16,24 @@ namespace TotalCommSpecflow
        // private Application app;
        // private Window mainwindow;
         private static readonly HttpClient client = new();
+        public static TotalComm total = new();
 
         //"https://www.totalcommander.ch/beta/tc1050x64_b1.exe"))
         // https://www.ghisler.com/
         public static async Task Main()
         {
-            using var stream = await client.GetStreamAsync(@"https://www.totalcommander.ch/beta/tc1050x64_b1.exe");
-            using var fileStream = new FileStream(@"C:\Users\User\Downloads\Automation\TotalCommand.exe", FileMode.CreateNew);
+            using var stream = await client.GetStreamAsync(@"https://www.totalcommander.ch/win/tcmd1000x64.exe");
+            using var fileStream = new FileStream(@"C:\Users\User\Downloads\TotalCommand.exe", FileMode.CreateNew);
             await stream.CopyToAsync(fileStream);
+            fileStream.Dispose();
+
+           total.InstallTheApp();
         }
 
+        public void InstallTheApp()
+        {
+            _ = System.Diagnostics.Process.Start(@"C:\Users\User\Downloads\TotalCommand.exe", "tcmd1000x64.exe /AHMGDU");
+        }
            
     }
 }
--- TotalCommAutomation/TotalCommSpecflow.spex/StepDefinitions/TotalCommStepDefinitions.cs
+++ TotalCommAutomation/TotalCommSpecflow.spex/StepDefinitions/TotalCommStepDefinitions.cs
@@ -20,7 +20,7 @@ namespace TotalCommSpecflow.spex.StepDefinitions
         [Then(@"instal the app")]
         public void ThenInstalTheApp()
         {
-            throw new PendingStepException();
+            total.InstallTheApp();
         }
 
 
