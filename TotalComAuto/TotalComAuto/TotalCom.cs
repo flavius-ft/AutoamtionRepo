0d09005fcd6ec74bc2918e78b52968350643de5c
Flaviu Tritean <flaviu.pk@gmail.com>
2022-05-12 15:22:25 +0300

    Path.Combine

--- TotalComAuto/TotalComAuto/TotalCom.cs
+++ TotalComAuto/TotalComAuto/TotalCom.cs
@@ -21,22 +21,19 @@ namespace TotalCommSpecflow
         // https://www.ghisler.com/
         public static async Task Main()
         {
-            string tempPath = Path.GetTempPath();
+            string tempPath = Path.Combine(Path.GetTempPath(), "TotalCommand.exe");
             using var stream = await client.GetStreamAsync(@"https://www.totalcommander.ch/win/tcmd1000x64.exe");
-            //  using var fileStream = new FileStream(@"C:\Users\User\Downloads\TotalCommand.exe", FileMode.CreateNew);
-            using var fileStream = new FileStream(tempPath + "TotalCommander.exe", FileMode.CreateNew);
+            using var fileStream = new FileStream(tempPath, FileMode.CreateNew);
             await stream.CopyToAsync(fileStream);
             fileStream.Dispose();
 
             Process process = new();
-            process.StartInfo.FileName = tempPath + "TotalCommander.exe";
+            process.StartInfo.FileName = tempPath;
             process.StartInfo.Arguments = "/AHMGDU";
             process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             process.Start();
             process.WaitForExit();
-
         }
-
 
 
 
